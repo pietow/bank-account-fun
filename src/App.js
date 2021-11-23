78d6e8d652aa6b608c5f6b4c3585ea799b4aa81e
piet <pprotassow@posteo.de>
2021-11-23 11:05:03 +0100

    reduced code with curried function

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import { useState } from 'react'
 /* import './App.css' */
-import './assets/main.css';
+import './assets/main.css'
 
-const BankInput = ({ balance, onBalanceChange, name, addOrMinus }) => {
+const BankInput = ({ balance, onBalanceChange, name }) => {
     const [input, setInput] = useState('')
 
     function handleInput(e) {
@@ -10,17 +10,23 @@ const BankInput = ({ balance, onBalanceChange, name, addOrMinus }) => {
     }
 
     function handleChange() {
-        const result = addOrMinus(balance, parseInt(input))
-        if (!Number.isNaN(result)) {
-                onBalanceChange(result)	
-                localStorage.balance = result
-        }  
+        onBalanceChange(balance, input)
     }
 
     return (
         <fieldset className="m-2 flex">
-            <input className="border-2 border-black rounded mr-3" value={input} onChange={handleInput} />
-            <button className="text-xl bg-green-500 text-white pr-1 pl-1 rounded-md w-2/6 shadow-xl transform hover:translate-y-1" tabIndex="-1" onClick={handleChange}>{name}</button>
+            <input
+                className="border-2 border-black rounded mr-3"
+                value={input}
+                onChange={handleInput}
+            />
+            <button
+                className="text-xl bg-green-500 text-white pr-1 pl-1 rounded-md w-2/6 shadow-xl transform hover:translate-y-1"
+                tabIndex="-1"
+                onClick={handleChange}
+            >
+                {name}
+            </button>
         </fieldset>
     )
 }
@@ -29,24 +35,37 @@ const add = (x, y) => x + y
 const minus = (x, y) => x - y
 
 function App() {
-    const [balance, setBalance] = useState(parseInt(localStorage.balance) || 1000)
+    const [balance, setBalance] = useState(
+        parseInt(localStorage.balance) || 1000
+    )
+
+    function currySetBalance(func) {
+        return function(x, y) {
+            const result = func(x, parseInt(y))
+            if (!Number.isNaN(result)) {
+                setBalance(result)
+                localStorage.balance = result
+            }
+        }
+    }
+
     return (
         <div className="container mx-auto flex flex-col justify-items-center items-center">
-            <h2 className="font-sans text-4xl m-5">Your current balance is {balance} €</h2>
+            <h2 className="font-sans text-4xl m-5">
+                Your current balance is {balance} €
+            </h2>
             <div>
                 <BankInput
-                balance={balance}
-                onBalanceChange={setBalance}
-                name = "Deposite"
-                addOrMinus = {add}
-            ></BankInput>
-        <BankInput
-        balance={balance}
-        onBalanceChange={setBalance}
-        name = "Withdraw"
-        addOrMinus = {minus}
-    ></BankInput>
-        </div>
+                    balance={balance}
+                    onBalanceChange={currySetBalance(add)}
+                    name="Deposite"
+                ></BankInput>
+                <BankInput
+                    balance={balance}
+                    onBalanceChange={currySetBalance(minus)}
+                    name="Withdraw"
+                ></BankInput>
+            </div>
         </div>
     )
 }
